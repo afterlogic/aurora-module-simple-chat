a2950715268afee84c0dc84af8249df0eab0a893
nadine-shi <nadine@afterlogic.com>
2016-12-26 14:10:17 +0300

    Merge modules SimpleChat and SimpleChatWebclient

---
+++ js/manager.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = function (oAppData) {
+	var App = require('%PathToCoreWebclientModule%/js/App.js');
+	
+	if (App.getUserRole() === Enums.UserRole.NormalUser || App.getUserRole() === Enums.UserRole.Customer)
+	{
+		var
+			_ = require('underscore'),
+
+			TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+
+			Settings = require('modules/%ModuleName%/js/Settings.js'),
+			oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
+		;
+
+		Settings.init(oSettings);
+
+		return {
+			enableModule: Settings.enableModule,
+
+			/**
+			 * Registers settings tab of simple chat module before application start.
+			 * 
+			 * @param {Object} ModulesManager
+			 */
+			start: function (ModulesManager) {
+				ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/SettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+			},
+
+			/**
+			 * Returns list of functions that are return module screens.
+			 * 
+			 * @returns {Object}
+			 */
+			getScreens: function () {
+				var oScreens = {};
+				oScreens[Settings.HashModuleName] = function () {
+					return require('modules/%ModuleName%/js/views/MainView.js');
+				};
+				return oScreens;
+			},
+
+			/**
+			 * Returns object of header item view of simple chat module.
+			 * 
+			 * @returns {Object}
+			 */
+			getHeaderItem: function () {
+				var CHeaderItemView = require('%PathToCoreWebclientModule%/js/views/CHeaderItemView.js');
+
+				return {
+					item: new CHeaderItemView(TextUtils.i18n('%MODULENAME%/ACTION_SHOW_CHAT')),
+					name: Settings.HashModuleName
+				};
+			}
+		};
+	}
+	
+	return null;
+};
