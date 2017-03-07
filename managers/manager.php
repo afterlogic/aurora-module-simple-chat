a9391b7e3850cc54b859a1c9b55d8a4f4d3500fc
sash04ek <a.ovcharov@gmail.com>
2017-03-07 12:24:06 +0300

    Remove

--- managers/manager.php
+++ managers/manager.php
@@ -27,7 +27,7 @@
 class CApiSimpleChatManager extends \Aurora\System\Managers\AbstractManager
 {
 	/**
-	 * @var CApiEavManager
+	 * @var \Aurora\System\Managers\Eav\Manager
 	 */
 	public $oEavManager = null;
 	
